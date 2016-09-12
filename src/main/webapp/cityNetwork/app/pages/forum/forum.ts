0f5a08b88f5c4ef68117318434b2177c87a9c321
wulq <wulq@wlq>
2016-09-12 16:48:26 +0800

    创建项目

---
+++ src/main/webapp/cityNetwork/app/pages/forum/forum.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+
+@Component({
+  templateUrl: 'build/pages/forum/forum.html'
+})
+export class ForumPage {
+
+  bannerHeight: string;
+  constructor(private navCtrl: NavController) {
+
+         
+
+  }
+}
--- src/main/webapp/cityNetwork/app/pages/tabs/tabs.ts
+++ src/main/webapp/cityNetwork/app/pages/tabs/tabs.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {HeadlinePage} from "../headline/headline";
+import {ForumPage} from "../forum/forum";
 
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html'
@@ -7,12 +8,13 @@ import {HeadlinePage} from "../headline/headline";
 export class TabsPage {
 
   private tab1Root: any;
+  private tab2Root: any;
 
 
   constructor() {
     // this tells the tabs component which Pages
     // should be each tab's root Page
     this.tab1Root = HeadlinePage;
-
+    this.tab2Root = ForumPage;
   }
 }
