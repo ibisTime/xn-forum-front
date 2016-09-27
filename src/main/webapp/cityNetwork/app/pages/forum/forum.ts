a9d07772f4fdf5a8a7db05df0e013fd927ea1528
wulq <wulq@wlq>
2016-09-27 19:45:11 +0800

    update

--- src/main/webapp/cityNetwork/app/pages/forum/forum.ts
+++ src/main/webapp/cityNetwork/app/pages/forum/forum.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
 import {NavController, Platform, Content} from 'ionic-angular';
 import {DetailPage} from "./detail/detail";
+import {HttpService} from "../../services/http.service";
 
 
 @Component({
@@ -13,14 +14,40 @@ export class ForumPage {
   imgHeight: string;
   pHeight: string;
   imgUrl: string;
+  start: number;
+  limit: number;
+  items = [];
 
   @ViewChild(Content) content: Content;
 
   constructor(private navCtrl: NavController,
-              private platform: Platform) {
+              private platform: Platform,
+              private http: HttpService) {
       this.isAndroid = platform.is('android');
       this.imgHeight = `${(this.platform.width()-16-50-16-16)/3 - 1}px`;
       this.pHeight = `${this.platform.height()}px`;
+      this.start = 1;
+      this.limit = 10;
+      this.queryPostPage();
+  }
+  queryPostPage(){
+      this.http.post('/post/page',{
+          "start": this.start,
+          "limit": this.limit
+        })
+        .then((res) => {
+            if(res.success){
+                let list = res.data.list;
+                for(let i = 0; i < list.length; i++){
+                    list.pic = list.pic.split(/\|\|/);
+                }
+                this.items.push(list);
+            }
+            
+            //loading.dismiss();
+        }).catch(error => {
+            //loading.dismiss();
+        });
   }
   showImg(ev){
       if( ev.target.nodeName.match(/^img$/i) ){
