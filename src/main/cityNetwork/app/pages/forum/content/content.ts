356b2bc4b4ca9f0a479f654a79b5fabb25d23315
wulq <wulq@wlq>
2016-09-29 14:35:35 +0800

    日期

--- src/main/cityNetwork/app/pages/forum/content/content.ts
+++ src/main/cityNetwork/app/pages/forum/content/content.ts
@@ -57,12 +57,12 @@ export class ContentPage {
         d_days = d/86400;       
         d_hours = d/3600;       
         d_minutes = d/60;       
-        if(d_days>0 && d_days<4){       
-            return d_days+"天前";       
-        }else if(d_days<=0 && d_hours>0){       
-            return d_hours+"小时前";       
-        }else if(d_hours<=0 && d_minutes>0){       
-            return d_minutes+"分钟前";       
+        if(d_days>1 && d_days<4){       
+            return Math.ceil(d_days)+"天前";       
+        }else if(d_days<1 && d_hours>0){       
+            return Math.ceil(d_hours)+"小时前";       
+        }else if(d_hours<1 && d_minutes>0){       
+            return Math.ceil(d_minutes)+"分钟前";      
         }else{       
             var s = new Date(publishTime*1000);       
             // s.getFullYear()+"年";
