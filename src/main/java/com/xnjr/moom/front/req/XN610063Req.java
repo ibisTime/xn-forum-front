d956b9de24e6b7431486ee19fe0de110517114fc
luoqi14 <luoqi_14@126.com>
2016-10-13 16:38:59 +0800

    草稿接口

---
+++ src/main/java/com/xnjr/moom/front/req/XN610063Req.java
@@ -0,0 +1,65 @@
+package com.xnjr.moom.front.req;
+
+public class XN610063Req {
+	
+	private String code;
+    
+	private String title;
+	
+	private String content;
+	
+	private String pic;
+	
+	private String plateCode;
+	
+	private String publisher;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getPic() {
+		return pic;
+	}
+
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
+
+	public String getPlateCode() {
+		return plateCode;
+	}
+
+	public void setPlateCode(String plateCode) {
+		this.plateCode = plateCode;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+}
