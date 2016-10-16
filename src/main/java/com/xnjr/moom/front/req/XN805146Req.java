06b6629d1c1d5b4a68ad9ce022e8e7a5611aa125
luoqi14 <luoqi_14@126.com>
2016-10-16 17:20:43 +0800

    阅读接口

---
+++ src/main/java/com/xnjr/moom/front/req/XN805146Req.java
@@ -0,0 +1,40 @@
+package com.xnjr.moom.front.req;
+
+public class XN805146Req {
+    private String smsCode;
+    private String status;
+    private String userId;
+    private String dateStart;
+    private String dateEnd;
+    
+	public String getSmsCode() {
+		return smsCode;
+	}
+	public void setSmsCode(String smsCode) {
+		this.smsCode = smsCode;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getDateStart() {
+		return dateStart;
+	}
+	public void setDateStart(String dateStart) {
+		this.dateStart = dateStart;
+	}
+	public String getDateEnd() {
+		return dateEnd;
+	}
+	public void setDateEnd(String dateEnd) {
+		this.dateEnd = dateEnd;
+	}
+}
