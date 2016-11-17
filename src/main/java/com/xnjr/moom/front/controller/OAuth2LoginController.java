4862fb5dde7ac3035848890797c965748312633c
luoqi14 <luoqi_14@126.com>
2016-11-17 20:17:38 +0800

    weibo登录

--- src/main/java/com/xnjr/moom/front/controller/OAuth2LoginController.java
+++ src/main/java/com/xnjr/moom/front/controller/OAuth2LoginController.java
@@ -20,6 +20,7 @@ import com.xnjr.moom.front.ao.IUserAO;
 import com.xnjr.moom.front.http.BizConnecter;
 import com.xnjr.moom.front.http.JsonUtils;
 import com.xnjr.moom.front.http.PostSimulater;
+import com.xnjr.moom.front.localToken.TokenDO;
 import com.xnjr.moom.front.session.SessionUser;
 import com.xnjr.moom.front.util.HttpKit;
 
@@ -44,7 +45,7 @@ public class OAuth2LoginController extends BaseController {
     @ResponseBody
     @SuppressWarnings({ "rawtypes", "unchecked" })
     public Object wxLogin(@RequestParam Map<String,String> map,
-    		HttpServletRequest request) throws Exception {
+    		HttpServletRequest request) {
     	// Step1：从回调URL中获取Authorization Code
     	String authCode = (String)map.get("code");
     	if (StringUtils.isBlank(authCode)) {
@@ -60,63 +61,80 @@ public class OAuth2LoginController extends BaseController {
 		formProperties.put("client_id", APP_ID);
 		formProperties.put("client_secret", APP_KEY);
 		formProperties.put("code", authCode);
-		formProperties.put("redirect_uri", request.getRequestURL().toString().replace("auth2/login/wx", "system/version.htm"));
-		String response = PostSimulater.requestPostForm(GET_TOKEN_URL,
-            formProperties);
-		res = getMapFromResponse(response);
-		accessToken = res.get("access_token");
-		if (res.get("error") != null || StringUtils.isBlank(accessToken)) {
-			return false;
+		String referer = request.getHeader("Referer");
+		formProperties.put("redirect_uri", referer.split("[?]")[0]);
+		System.out.println("-----------" + referer.split("[?]")[0]);
+		String response;
+		try {
+			response = PostSimulater.requestPostForm(GET_TOKEN_URL,
+			    formProperties);
+			res = getMapFromResponse(response);
+			accessToken = res.get("access_token");
+			if (res.get("error") != null || StringUtils.isBlank(accessToken)) {
+				return false;
+			}
+			// Step3：使用Access Token来获取用户的OpenID
+			String openId = "";
+			String userId = "";
+			openId = res.get("uid");
+//			queryParas = new HashMap<>(1);
+//			queryParas.put("access_token", accessToken);
+//			res = getMapFromResponse(HttpKit.get(GET_OPENID_URL, queryParas));
+//			openId = res.get("openid");
+//			if (res.get("error") != null || StringUtils.isBlank(openId)) {
+//				return "redirect:/";
+//			}
+			// Step4：根据openId从数据库中查询用户信息（user）
+			res = new HashMap<>();
+			res.put("openId", openId);
+			Map[] users = BizConnecter.getBizData("805055", JsonUtils.mapToJson(res),
+		              Map[].class);
+			Map<String, String> user = new HashMap<>();
+			if (users.length != 0) {
+				// Step4-1：如果user存在，说明用户授权登录过，直接登录
+				user = users[0];
+				userId = user.get("userId");
+				SessionUser sessionUser = new SessionUser();
+		        sessionUser.setUserId(userId);
+		        // 创建session
+		        setSessionUser(sessionUser);
+		        TokenDO tokenDO = new TokenDO();
+		        tokenDO.setUserId(userId);
+		        tokenDO.setTokenId(pwdUserId(userId));
+				return tokenDO;
+			} else {
+				// Step4-2：如果user不存在，说明用户未授权登录过，需从openAPI获取用户信息
+				queryParas = new HashMap<>();
+				queryParas.put("access_token", accessToken);
+				queryParas.put("uid", openId);
+//				queryParas.put("oauth_consumer_key", APP_ID);
+//				queryParas.put("openid", openId);
+				res = getMapFromResponse(HttpKit.get(GET_USER_INFO_URL, queryParas));
+				String name = (String)res.get("screen_name");
+				
+				user.put("openId", openId);
+				user.put("nickname", name);
+				user.put("gender", (res.get("gender").toString().equals("m")) ? "1" : "0");
+				user.put("photo", res.get("avatar_large"));
+				user = BizConnecter.getBizData("805151", JsonUtils.mapToJson(user),
+			              Map.class);
+				userId = user.get("userId");
+				SessionUser sessionUser = new SessionUser();
+		        sessionUser.setUserId(userId);
+		        // 创建session
+		        setSessionUser(sessionUser);
+		        TokenDO tokenDO = new TokenDO();
+		        tokenDO.setUserId(userId);
+		        tokenDO.setTokenId(pwdUserId(userId));
+				return tokenDO;
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		// Step3：使用Access Token来获取用户的OpenID
-		String openId = "";
-		String userId = "";
-		openId = res.get("uid");
-//		queryParas = new HashMap<>(1);
-//		queryParas.put("access_token", accessToken);
-//		res = getMapFromResponse(HttpKit.get(GET_OPENID_URL, queryParas));
-//		openId = res.get("openid");
-//		if (res.get("error") != null || StringUtils.isBlank(openId)) {
-//			return "redirect:/";
-//		}
-		// Step4：根据openId从数据库中查询用户信息（user）
-		res = new HashMap<>();
-		res.put("openId", openId);
-		Map[] users = BizConnecter.getBizData("805055", JsonUtils.mapToJson(res),
-	              Map[].class);
-		Map<String, String> user = new HashMap<>();
-		if (users.length != 0) {
-			// Step4-1：如果user存在，说明用户授权登录过，直接登录
-			user = users[0];
-			userId = user.get("userId");
-			SessionUser sessionUser = new SessionUser();
-	        sessionUser.setUserId(userId);
-	        // 创建session
-	        setSessionUser(sessionUser);
-			return pwdUserId(userId);
-		} else {
-			// Step4-2：如果user不存在，说明用户未授权登录过，需从openAPI获取用户信息
-			queryParas = new HashMap<>();
-			queryParas.put("access_token", accessToken);
-			queryParas.put("uid", openId);
-//			queryParas.put("oauth_consumer_key", APP_ID);
-//			queryParas.put("openid", openId);
-			res = getMapFromResponse(HttpKit.get(GET_USER_INFO_URL, queryParas));
-			String name = (String)res.get("screen_name");
-			
-			user.put("openId", openId);
-			user.put("nickname", name);
-			user.put("gender", (res.get("gender").toString().equals("m")) ? "1" : "0");
-			user.put("photo", res.get("avatar_large"));
-			user = BizConnecter.getBizData("805151", JsonUtils.mapToJson(user),
-		              Map.class);
-			userId = user.get("userId");
-			SessionUser sessionUser = new SessionUser();
-	        sessionUser.setUserId(userId);
-	        // 创建session
-	        setSessionUser(sessionUser);
-			return pwdUserId(userId);
-		}
+		
+		return false;
+		
 	}
     
     
